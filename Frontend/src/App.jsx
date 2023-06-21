fe8148956daba4be9bf95059e2119f242b51941d
Shivam Varshney <12111043@nitkkr.ac.in>
2023-06-21 12:35:23 +0530

    Folder Structure Updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Home from "./components/home/Home";
-import HoverPage from "./Canvas/Hover";
+import HoverPage from "./Canvas/Hover/Hover";
 
 
 function App() {
